53556e772767c241b0a2d0628e392ea5b981ffc0
ahern88 <ahern88@163.com>
2015-05-07 22:50:14 +0800

    init play study

---
+++ playstudy/app/controllers/Application.java
@@ -0,0 +1,19 @@
+package controllers;
+import play.mvc.Controller;
+
+public class Application extends Controller {
+	
+	/**
+	 * 首页
+	 */
+	public static void index(){
+		render();
+	} 
+	
+	/**
+	 * 登陆页
+	 */
+	public static void login(){
+		render();
+	}
+}
