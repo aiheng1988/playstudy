8caa1f1a540a3cb3fa40a4b0d463a332e4981ce1
ahern88 <ahern88@163.com>
2015-05-09 23:47:17 +0800

    add admin page

--- playstudy/app/controllers/Application.java
+++ playstudy/app/controllers/Application.java
@@ -1,7 +1,26 @@
 package controllers;
+import play.data.validation.Validation;
+import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Application extends Controller {
+	
+	/**
+	 * 检查用户是否登陆
+	 */
+	@Before(unless={"login","auth"})
+    static void checkAuthenticated() {
+        if(!session.contains("userName")) {
+            login();
+        }
+    }
+	
+	@Before(only={"login"})
+	static void isLogined() {
+		if(session.contains("userName")) {
+            admin();
+        }
+	}
 	
 	/**
 	 * 首页
@@ -15,5 +34,39 @@ public class Application extends Controller {
 	 */
 	public static void login(){
 		render();
+	}
+	
+	/**
+	 * 验证用户名和密码
+	 */
+	public static void auth(String userName, String password){
+		validation.required(userName);
+		validation.required(password);
+		if(validation.hasErrors()) {
+			params.flash(); 
+	        validation.keep();
+	        login();
+		}
+		if("admin".equals(userName) && "admin".equals(password)) {
+			session.put("userName", userName);
+			admin();
+		} else {
+			Validation.addError("userNameAndPassword", "error");
+			params.flash(); 
+			validation.keep();
+			login();
+		}
+	}
+	
+	public static void admin(){
+		render();
+	}
+	
+	/**
+	 * 退出登陆
+	 */
+	public static void logout(){
+		session.clear();
+		login();
 	}
 }
